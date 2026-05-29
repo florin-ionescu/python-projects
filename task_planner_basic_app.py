e088a069124197c17a7951bb09c27a517edf2522
Florin Ionescu <126186642+Cichicean@users.noreply.github.com>
2026-05-29 11:16:14 +0300

    Task planner basic up finishing touches

--- task_planner_basic_app.py
+++ task_planner_basic_app.py
@@ -3,16 +3,20 @@
 tasks = []
 
 def task_planner():
+    # Menu creating - use while loops to go over the code until you tell it to stop
     while True:
         print("\nTask planner\n")
         print("Choose an option: ")
         print("1. Add a task")
         print("2. View all task")
         print("3. Exit")
+
+        # avoids getting the error from typing something incorrect (it must be int)
         try:
             choice = int(input("\nPlease choose an option from the menu: "))
         except ValueError:
             print("Invalid option. Please enter a number: 1, 2 or 3 ")
+            continue
 
         if choice == 1:
             task = input("What is your task, master? ")
@@ -25,7 +29,7 @@ def task_planner():
                 print("\nYour tasks:")
                 for task in tasks:
                     print("-", task)
-        elif choice == 3:
+        elif choice == 3: # this breaks the loop
             print("Loop end")
             break
         else:
